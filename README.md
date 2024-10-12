156683ad20aaffc09b53eba26dfcc0fa7ff386ff
Lucas Chaves <lucasvchaves@gmail.com>
2024-10-12 14:29:32 -0300

    working on the libIO subroutines

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Este é um repositório de estudos de assembly, sei que tem muita coisa errada k
 
 Estou utilizando o livro "Programação em Baixo Nível" do autor Igor Zhirkov. Ele aborda assembly e C para arquitetura Intel x86-64.
 
+A pasta src possui alguns exercicios jogados. 
+Já a pasta libio possui uma biblioteca para IO básica. 
+
 ## Build
 Estou usando o nasm para montar o código assembly e o ld para linkar o código objeto gerado.
 
---
+++ libio/libio.s
@@ -0,0 +1,131 @@
+section .text
+
+exit:
+    xor rdi, rdi              ; exit code 0
+    mov rax, 60               ; exit syscall flag
+    syscall
+
+string_length:
+    xor rax, rax              ; zeroing rax to store str length
+.loop:
+    cmp byte [rdi + rax], 0   ; checks if current symbol = 0 (null terminator)
+    je .end                   ; jmp if true
+    inc rax                   ; increase counter
+    jmp .loop
+.end:
+    ret
+
+print_char:
+    push rdi                  ; pushes argument to stack
+    mov rdi, rsp              ; copies value from stackptr to rdi
+    call print_string
+    pop rdi                   ; pops value com stack
+    ret
+
+print_newline:
+    mov rdi, 10               ; 10 = new line symbol 
+    jmp print_char    
+
+print_string:
+    push rdi                  ; push arg to stack
+    call string_length        ; get str length
+    pop rsi
+    mov rdx, rax
+    mov rax, 1
+    mov rdi, 1
+    syscall
+    ret
+
+print_uint:
+    mov rax, rdi
+    mov rdi, rsp              ; value from stackptr to str (head of stack)
+    push 0                    ; push null terminator to stack
+    sub rsp, 10               ; reserves 16bytes to store 64bit unsigned int  
+
+    dec rdi                   ; moves rdi to the pos before '\0', opening space for the first char 
+                              ; of the str, allowing the chars to be stored back to front
+    mov r8, 10                ; 10 stands for decimal, will be used to divide and extract digits
+.loop:
+    xor rdx, rdx              ; zeroes rdx
+    div r8                    ; division, rax is quotient and rdx is the remainder wich represents
+                              ; the ASCII char of the current digit
+    or dl, 0x30               ; dl is the lesser byte of rdx, this instruction converts the hex of
+                              ; a 0-9 digit to their respective ASCII decimal number
+    dec rdi                   ; decreases the pointer to store the next digit
+    mov [rdi], dl             ; inserts the converted digit into the correct pos
+    test rax, rax             ; if the quotient (rax) is zero all numbers have been processed 
+    jnz .loop
+
+    call print_string
+
+    add rsp, 24               ; clears the stack from 24 used bytes: 8 from 'push 0' + 16 from
+                              ; 'sub rsp, 18'
+    ret
+
+print_int:
+    test rdi, rdi             ; test if int is signed
+    jns print_uint            ; if not signed jmp to print_uint
+    push rdi                  ; push int to stack
+    mov rdi, '-'              ; sign char to rdi (only negative ints are treated in this subrotine)
+    call print_char           ; prints the sign char
+    pop rdi                   ; pops sign char
+    neg rdi                   ; inverts value
+    jmp print_uint            ; prints the unsigned int
+
+; rdi points to a string
+; returns rax: number, rdx : length
+parse_int:
+    mov al, byte [rdi]        ; moves rdi pointer size to 'al'
+    cmp al, '-'               ; checks if is signed int
+    je .signed                ; if is signed this subrotine will process it
+    jmp parse_uint            ; if not uint subrotine will process it 
+.signed:
+    inc rdi                   ; increase pointer = step to next char in string
+    call parse_uint
+    neg rax                   ; invert value in rax
+    test rdx, rdx             ; test to check if zero
+    jz .error                 ; if zero jmp to error
+
+    inc rdx                   ; else increase rdx value (length)
+    ret
+.error:
+    xor rax, rax              ; zero rax
+    ret
+
+; rdi points to a string
+; returns rax: number, rdx : length
+parse_uint:
+    mov r8, 10                ; base 10 for decimal
+    xor rax, rax              ; zeroes rax to store final numeric value
+    xor rcx, rcx              ; zeroes rcx to store counter index
+.loop:
+    movzx r9, byte [rdi + rcx]; moves with 0 extention for 64 bits to r9 
+    cmp r9b, '0'              ; if char is less then 0 (non numeric digit)
+    jb .end                   ; jmp to end if below (unsigned less then)
+    cmp r9b, '9'              ; if char is bigger then 9 (non numeric digit)
+    ja .end                   ; jmp to end if above (unsigned bigger then)
+    xor rdx, rdx              ; clears rdx because mul uses rdx:rax as input
+    mul r8                    ; multiplies rax by 10 (content of r8)
+    and r9b, 0x0f             ; extracts the numeric value of the ASCII char
+    add rax, r9               ; updates the accumulated numeric value with new digit
+    inc rcx                   ; steps to next digit
+    jmp .loop
+.end:
+    mov rdx, rcx              ; returns the length in rdx
+    ret
+
+string_equals:
+    xor rax, rax
+    ret
+
+
+read_char:
+    xor rax, rax
+    ret 
+
+read_word:
+    ret
+
+
+string_copy:
+    ret
